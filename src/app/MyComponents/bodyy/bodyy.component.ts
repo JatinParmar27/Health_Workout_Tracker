af5146b2e989d4166369133afa36f19037abbf78
Jatin Parmar <jatinrparmar2003@gmail.com>
2024-07-27 22:22:05 +0530

    Add workout page is designed

---
+++ src/app/MyComponents/bodyy/bodyy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bodyy',
+  standalone: true,
+  imports: [],
+  templateUrl: './bodyy.component.html',
+  styleUrl: './bodyy.component.css'
+})
+export class BodyyComponent {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./MyComponents/nav/nav.component";
+import { BodyyComponent } from "./MyComponents/bodyy/bodyy.component";
+import { AddWorkoutComponent } from "./MyComponents/add-workout/add-workout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, BodyyComponent, AddWorkoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
